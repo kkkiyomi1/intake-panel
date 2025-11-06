203e0a854a88b0512422a1b9494e8ba7afe8b5c3
Yi Zhu <126227403+kkkiyomi1@users.noreply.github.com>
2025-11-06 16:17:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+// vite.config.ts
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// 在 ESM 下手动模拟 __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "src") }
+    alias: { "@": path.resolve(__dirname, "./src") }
   }
 });
+
