34a0bd2c5f7d4f7b51be959d53ce59c209d43700
Yi Zhu <126227403+kkkiyomi1@users.noreply.github.com>
2025-11-29 21:26:03 +0800

    Allow weight field in Firestore rules

---
+++ types/external.d.ts
@@ -0,0 +1,23 @@
+declare module "vite-plugin-pwa";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms?: string[];
+  readonly userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
+  prompt(): Promise<void>;
+}
+
+declare global {
+  interface WindowEventMap {
+    beforeinstallprompt: BeforeInstallPromptEvent;
+  }
+}
+
+declare module "virtual:pwa-register" {
+  export interface RegisterSWOptions {
+    immediate?: boolean;
+    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
+    onRegisterError?: (error: any) => void;
+  }
+
+  export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
+}
